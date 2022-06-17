905b97c7808de093ebe1d85c8f01ec5568e67d45
uchuujin-dev <46563482+uchuujin-dev@users.noreply.github.com>
2022-06-17 12:54:31 +0100

    Save amount for widgets User is now able to edit and save amounts for pots and net worth widgets. Years to fire needs something to stop it from flipping

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,31 +1,40 @@
 import React, { useState } from "react";
 import "./widget.scss";
+import { InputGroup, FormControl } from "react-bootstrap";
 
-const Widget = ({ widget }) => {
+const Widget = ({ widget, saveWidgetText }) => {
   const [toggleFlip, setToggleFlip] = useState(false);
+  const [input, setInput] = useState({});
 
-  function isFlipped() {
+  function handleInput(event) {
+    const { name, value } = event.target;
+
+    setInput((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
+
+  function flipWidget() {
     const currentState = toggleFlip;
     setToggleFlip(!currentState);
   }
   return (
-    <div
-      className={toggleFlip ? "widget is_flipped" : "widget"}
-      onClick={() => isFlipped()}
-    >
-      <div className="widget__front">
+    <div className={toggleFlip ? "widget is_flipped" : "widget"}>
+      <div className="widget__front" onClick={() => setToggleFlip(true)}>
         <div className="left">
           <span className="title">{widget.title}</span>
           {widget.text.map((text) => {
             return (
               <div>
-                <span className="counter">{text.title}</span> &nbsp;
+                <span className="counter">{text.title}</span>
                 <span className="counter">{text.content}</span>
               </div>
             );
           })}
 
-          <span className="link">{widget.link}</span>
+          <span className="link" onClick={() => flipWidget()}>
+            {widget.link}
+          </span>
         </div>
         <div className="right">{widget.icon}</div>
       </div>
@@ -36,13 +45,33 @@ const Widget = ({ widget }) => {
           {widget.text.map((text) => {
             return (
               <div>
-                <span className="counter">{text.title}</span> &nbsp;
-                <span className="counter">{text.content}</span>
+                <label htmlFor={text.title} className="counter">
+                  {text.title}
+                </label>
+
+                <InputGroup className="mb-3">
+                  <InputGroup.Text>£ </InputGroup.Text>
+                  <FormControl
+                    name={text.id}
+                    type="number"
+                    placeholder={text.content}
+                    onChange={handleInput}
+                    aria-label="Pound amount"
+                  />
+                </InputGroup>
               </div>
             );
           })}
 
-          <span className="link">{widget.link}</span>
+          <span
+            className="link"
+            onClick={() => {
+              saveWidgetText(widget.id, input);
+              flipWidget();
+            }}
+          >
+            SAVE
+          </span>
         </div>
         <div className="right">{widget.icon}</div>
       </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,17 +11,19 @@ const Home = () => {
   const [widgetText, setWidgetText] = useState([
     {
       id: "fire",
+      position: 1,
       title: "YEARS TO FIRE",
-      text: [{ id: "fire", title: "?", content: "" }],
+      text: [{ id: "fire", title: "", content: "?" }],
       link: "Calculate",
       icon: <LocalFireDepartmentRoundedIcon className="icon" />
     },
     {
       id: "pots",
+      position: 2,
       title: "POTS",
       text: [
-        { id: "emergency", title: "Emergency fund", content: ": ?" },
-        { id: "cash", title: "Other cash", content: ": ?" }
+        { id: "emergency", title: "Emergency fund: ", content: "?" },
+        { id: "cash", title: "Other cash: ", content: "?" }
       ],
 
       link: "Click to edit",
@@ -29,16 +31,49 @@ const Home = () => {
     },
     {
       id: "worth",
+      position: 3,
       title: "NET WORTH",
       text: [
-        { id: "assets", title: "Assets", content: ": ?" },
-        { id: "liabilities", title: "Liabilities", content: ": ?" }
+        { id: "assets", title: "Assets: ", content: "?" },
+        { id: "liabilities", title: "Liabilities: ", content: "?" }
       ],
       total: "?",
       link: "Click to edit",
       icon: <AutoGraphRoundedIcon className="icon" />
     }
   ]);
+  function sortByPosition(a, b) {
+    if (b.position) {
+      return a.position > b.position ? 1 : a.position < b.position ? -1 : 0;
+    } else {
+      return 0;
+    }
+  }
+
+  function saveWidgetText(widgetId, newText) {
+    setWidgetText((prev) => {
+      let newWidgetTextObj = prev.find((e) => e.id === widgetId);
+      let oldObjArr = prev.filter((obj) => obj !== newWidgetTextObj);
+
+      const newTextKeys = Object.keys(newText);
+
+      const textArr = newWidgetTextObj.text.map((text) => {
+        const key = newTextKeys.find((key) => key === text.id);
+        if (text.id === key) {
+          return {
+            ...text,
+            content: newText[key]
+          };
+        } else {
+          return text;
+        }
+      });
+
+      newWidgetTextObj = { ...newWidgetTextObj, text: textArr };
+
+      return [...oldObjArr, newWidgetTextObj].sort(sortByPosition);
+    });
+  }
 
   return (
     <div className="home">
@@ -47,7 +82,7 @@ const Home = () => {
         <Navbar />
         <div className="widgets">
           {widgetText.map((widget) => {
-            return <Widget widget={widget} />;
+            return <Widget widget={widget} saveWidgetText={saveWidgetText} />;
           })}
         </div>
         <div className="charts"></div>
