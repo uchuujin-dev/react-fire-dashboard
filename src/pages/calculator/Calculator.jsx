cd873bba554ec72e46cdb2bcb73da33d38395043
uchuujin-dev <46563482+uchuujin-dev@users.noreply.github.com>
2022-06-19 16:59:11 +0100

    Add calculator UI

--- src/pages/calculator/Calculator.jsx
+++ src/pages/calculator/Calculator.jsx
@@ -1,10 +1,53 @@
 import React from "react";
 import "./calculator.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import CalculateRoundedIcon from "@mui/icons-material/CalculateRounded";
 
 const Calculator = () => {
   return (
     <div className="calculator">
-      <h1 className="title">Calculator</h1>
+      <Sidebar />
+      <div className="calcContainer">
+        <Navbar />
+        <div className="mainSection">
+          <div className="header">
+            <h1 className="title">Calculator</h1>
+          </div>
+          <div className="form">
+            <article className="inputGroup">
+              <label htmlFor="">Annual expenses</label>
+              <input type="text" />
+            </article>
+            <article className="inputGroup">
+              <label htmlFor="">Withdrawal rate</label>
+              <input type="text" />
+            </article>
+            <article className="inputGroup">
+              <label htmlFor="">Initial balance</label>
+              <input type="text" />
+            </article>
+            <article className="inputGroup">
+              <label htmlFor="">Annual growth</label>
+              <input type="text" />
+            </article>
+            <article className="inputGroup">
+              <label htmlFor="">Years</label>
+              <input type="text" />
+            </article>
+            <article className="inputGroup">
+              <label htmlFor="">
+                Deposit monthly <span className="optional">(optional)</span>
+              </label>
+              <input type="text" />
+            </article>
+
+            <button className="calcBtn">
+              <CalculateRoundedIcon className="calcBtnIcon" fontSize="large" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
